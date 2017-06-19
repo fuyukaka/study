9b8b25d1d6972b92195d4e17a2cd0915c22ba85e
fuyukaka <fuyukaka@fuyukaka-001.fuji.fsi.local>
2017-06-19 15:02:09 +0900

    test

---
+++ kadai/newdateForm.php
@@ -0,0 +1,24 @@
+<!cDOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<title>挿入入力画面</title>
+</head>
+<body>
+<form action="newdate.php" method="post">
+<br><br>ID
+<br><input type="text" name="id" value="" />
+<br><br>商品名
+<br><input type="text" name="items_name" value="" />
+<br><br>価格
+<br><input type="text" name="price" value="" />
+<br><br>キーワード
+<br><input type="text" name="keyword" value="" />
+
+<br><input type="submit" value="送信する"/>
+
+</form>
+
+</body>
+</html>
